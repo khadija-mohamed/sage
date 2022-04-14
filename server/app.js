9ef398eeb7a0db89a1a8da56eafeab59a68287b3
Khadija Mohamed <khadijam@live.ca>
2022-04-14 19:56:46 -0400

    init

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const db = require('./configs/db.config');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -15,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter());
+
 
 module.exports = app;
