304c9f34cdbbee3494d43e4f7e49bf09064179c8
Carmen Kuang <carmen@Carmens-MacBook-Air.local>
2022-04-18 17:05:39 -0400

    create tables and insert data

--- server/db/schema/02_appointments.sql
+++ server/db/schema/02_appointments.sql
@@ -1,11 +1,12 @@
--- schema/03_appointments.sql
+-- schema/02_appointments.sql
 DROP TABLE IF EXISTS appointments CASCADE;
 
 -- CREATE appointments
 CREATE TABLE appointments (
   id SERIAL PRIMARY KEY NOT NULL,
-  time TIMESTAMP,
-  created_at TIMESTAMP,
+  appointment_date DATE,
+  appointment_time VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   mentee_id INTEGER REFERENCES mentees(id) ON DELETE CASCADE,
   mentor_id INTEGER REFERENCES mentors(id) ON DELETE CASCADE
 );
