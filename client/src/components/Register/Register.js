911e2b3b6b8a3a38487d967df959b580d9f98794
carmenkk <carmenkuang87@gmail.com>
2022-04-18 21:30:56 -0400

    Create folders for login and register views

---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Register.css';
+
+export default function Register() {
+  return(
+    <div className="login-wrapper">
+      <h1>Welcome to Sage</h1>
+      <form>
+        <label>
+          <p>First Name</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Last Name</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Email</p>
+          <input type="email" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <label>
+          <p>Confirm Password</p>
+          <input type="password" />
+        </label>
+        <label>
+          <p>Upload a Photo</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>About</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Skills</p>
+          <input type="text" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
