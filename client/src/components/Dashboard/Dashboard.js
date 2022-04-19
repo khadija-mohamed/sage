1ec57c4e39a29681b81da4d5181a0e9b3736e951
carmenkk <carmenkuang87@gmail.com>
2022-04-18 22:17:41 -0400

    Add a dashboard component with a basic layout

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Dashboard.css';
+
+export default function Dashboard() {
+  return(
+    <div className="dashboard-wrapper">
+      <h1>My Feed</h1>
+    </div>
+  )
+}
