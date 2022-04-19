a1fb326705ea7292cfddd3076c42f2be4bbe1a0b
carmenkk <carmenkuang87@gmail.com>
2022-04-18 21:29:54 -0400

    Set up Login component with basic layout

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import "components/login.scss";
+import './Login.css';
 
-export default function Button(props) {
-  
-  });
+export default function Login({ setToken }) {
+  return(
+    <div className="login-wrapper">
+      <h1>Sage Log In</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
-  return (
-    <Form
-      className={buttonClass}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-     
-    </Form>
-  );
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
