715fd0b1514ecddb1ef774a73cb94e1cf9a4dc20
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-18 21:03:22 -0400

    Set up routes for

---
+++ client/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "components/login.scss";
+
+export default function Button(props) {
+  
+  });
+
+  return (
+    <Form
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+     
+    </Form>
+  );
+}
