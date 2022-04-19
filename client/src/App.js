92ca7b42645107356890231177e1bc5651e0cf71
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-18 21:28:55 -0400

    Import the Login component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect } from 'react';
+import Login from '../src/components/Login/Login';
 
 function App() {
 
