f40786075817f29733f3336d0bdbb7ba83600501
carmenkk <carmenkuang87@gmail.com>
2022-04-21 15:46:30 -0400

    Add a get and post route for filter

--- server/routes/filter.js
+++ server/routes/filter.js
@@ -7,13 +7,38 @@ const getAllMentees = (db, options) => {
 
   if (options.skill) {
     queryParams.push(`%${options.skill}%`);
-    queryString += ` WHERE skill LIKE $${queryParams} `;
+    queryString += `WHERE skill LIKE $${queryParams};`;
   }
-  return db.query(queryString, queryParams).then((res) => {
-    return res.rows;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  return db
+    .query(queryString, queryParams)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllMentees = getAllMentees;
+
+module.exports = (db) => {
+  router.get("/dashboard/menteefind", (req, res) => {
+    getAllMentees(db, req.url)
+      .then((values) => res.send({ values }))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  // router.post("/users", (req, res) => {
+  //   const userId = req.session.userId;
+  //   const options = req.body;
+    
+  //   getAllMentees(db, req.url)
+  //     .then((values) => res.send({ values }))
+  //     .catch((e) => {
+  //       console.error(e);
+  //       res.send(e);
+  //     });
+  // });
+  return router;
+};
