fe7d286dfec54a37181154eb6966cad4a088aa0d
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-21 17:19:53 -0400

    Create components for MenteeList and MenteeListItem

---
+++ client/src/components/Dashboard/Mentee/MenteeList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./MentorList.css";
+import MenteeListItem from "./MenteeListItem";
+
+
+export default function MenteeList(props) {
+
+  const mentees = props.mentees.map((mentee) => {
+    return (
+      <MenteeListItem
+        key={mentee.id}
+        firstname={mentee.first_name}
+        lastname={mentee.last_name}
+
+        photo={mentee.photo_url}
+        skill={mentee.skill}
+        description={mentee.description}
+        location={mentee.location}
+      />
+    );
+  });
+
+  return (
+    <section className="mentees">
+      <h4 className="mentees__header text--light">Mentees</h4>
+      <ul className="mentees__list">{mentees}</ul>
+    </section>
+  );
+}
---
+++ client/src/components/Dashboard/Mentee/MenteeListItem.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./MenteeListItem.css";
+
+export default function MenteeListItem(props) {
+ 
+  return (
+    <div className='card'>
+      <div className="card_photo">
+        <img src={props.photo_url} 
+              alt=''/>
+      </div>
+        <h3>{props.first_name} {props.last_name}</h3>
+      
+      <div className="card_description">
+        <p>{props.description}</p>
+        
+      </div>
+      <div className="card_tag">
+        <p>{props.skill}</p>
+        
+      </div>
+      <div className="card_tag">
+        <p>{props.location}</p>
+        
+      </div>
+        
+      <div>
+        <button className="card_btn">view profile</button>
+      </div>
+      
+    </div>
+  );
+}
