7d4e0917384e1d43ba5f7b33cc4200763e281698
carmenkk <carmenkuang87@gmail.com>
2022-04-20 20:45:14 -0400

    modify database and seeds

--- server/db/seeds/02_appointments.sql
+++ server/db/seeds/02_appointments.sql
@@ -1,12 +1,12 @@
 -- seeds/03_appointments.sql
 -- Seed appointments
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-22', '5:00pm', 1, 3);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-18', '5:30pm', 2, 1);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-20', '2:00pm', 3, 5);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-28', '12:30pm', 4, 2);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-22', '4:00pm', 5, 3);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-24', '5:00pm', 1, 4);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-20', '6:30pm', 2, 3);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-17', '7:00pm', 1, 5);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-23', '11:30am',3, 2);
-INSERT INTO appointments (appointment_date, appointment_time, mentee_id, mentor_id) VALUES ('2022-04-27', '3:00pm', 4, 1);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-22', '5:00pm', 'react component', 1, 3);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-18', '5:30pm', 'hey', 2, 1);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-20', '2:00pm', 'Job interview', 3, 5);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-28', '12:30pm', 'hi', 4, 2);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-22', '4:00pm', 'code review', 5, 3);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-24', '5:00pm', 'code discussion', 1, 4);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-20', '6:30pm', 'interview advice', 2, 3);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-17', '7:00pm', 'heyy', 1, 5);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-23', '11:30am','Job interview', 3, 2);
+INSERT INTO appointments (appointment_date, appointment_time, message, mentee_id, mentor_id) VALUES ('2022-04-27', '3:00pm', 'mock interview', 4, 1);
