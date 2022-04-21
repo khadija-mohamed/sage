f8c218102afbb1f8ae268eba36274f2be8e206c6
carmenkk <carmenkuang87@gmail.com>
2022-04-21 15:00:03 -0400

    mentorlist and useapplicationdata

---
+++ server/routes/search.js
@@ -0,0 +1,20 @@
+const router = require("express").Router({
+  mergeParams: true,
+});
+const pool = require("../configs/db.config");
+
+
+module.exports = (db) => {
+  router.get("/search", (req, res) => {
+    console.log("SEARCH BODY", req.query)
+    const sql = "SELECT * FROM mentors WHERE category_id = $1 AND name LIKE $2 AND price >= $3 AND price <= $4";
+    const params = [req.query.categories, "%"+req.query.itemName+"%", ((req.query.minPrice)*100), ((req.query.maxPrice)*100)];
+    db.query(sql, params)
+    .then(data => {
+      const templateVars = { items: data.rows, categories: req.query.categories, itemName: req.query.itemName }
+      console.log("search results", templateVars.items)
+      res.render("search", templateVars);
+    })
+  });
+  return router;
+};
