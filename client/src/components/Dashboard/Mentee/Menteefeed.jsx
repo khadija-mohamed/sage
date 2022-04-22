f8b5bcb39335f75e74d369fe31dfd6c2de32e01c
Carmen Kuang <carmen@Carmens-MacBook-Air.local>
2022-04-22 14:08:45 -0400

    create singlementor profile page

--- client/src/components/Dashboard/Mentee/Menteefeed.jsx
+++ client/src/components/Dashboard/Mentee/Menteefeed.jsx
@@ -1,35 +1,23 @@
 import React, { useState } from 'react';
 import './Menteefeed.css';
-import Dashboard from '../Dashboard';
+// import Dashboard from '../Dashboard';
 import Searchbar from '../Searchbar/Searchbar';
 import Grid from '../Grid/Grid';
 import Quotes from '../Quotes/Quotes';
-import Filter from '../Filter/Filter';
-import Block from '../Block/Block';
+// import Filter from '../Filter/Filter';
+// import Block from '../Block/Block';
 import useApplicationData from "../../../hooks/useApplicationData.js";
 import MentorList from '../Mentor/MentorList';
 import MentorListItem from '../Mentor/MentorListItem';
 
-// export default function Menteefeed(props) {
-//   const { state,setState} = useApplicationData();
-  
-//   return(
-//     <div className="menteefeed-wrapper">
-    
-//     <Grid />
-//     <Quotes />
-//     <Block/>
-//     <Dashboard/>
-//     <MentorList mentors = {state.mentors} setMentor ={setState}/>
-//     </div>
-//   )
-// }
+
 export default function Menteefeed(props) {
-  const { state} = useApplicationData();
-  const data = state.mentors;
+  
+  const data = props.state.mentors;
   const [allData, setData] = useState(data);
-  console.log("props",props)
-  console.log("state.mentors",state.mentors)
+  
+
+  
 
   const generateLocationDataForDropdown = () => {
     return [...new Set(data.map((item) => item.location))];
