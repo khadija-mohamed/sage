747ffb9fa89aa4c9e1eb092b4591bbe9f2ca4fbb
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-22 18:15:20 -0400

    Fix errors

--- client/src/components/Dashboard/Mentor/MentorListItem.jsx
+++ client/src/components/Dashboard/Mentor/MentorListItem.jsx
@@ -1,79 +1,26 @@
-import React, { useState } from 'react';
-// import Dashboard from '../Dashboard';
-import Searchbar from '../Searchbar/Searchbar';
-import Grid from '../Grid/Grid';
-import Quotes from '../Quotes/Quotes';
-import useApplicationData from "../../../hooks/useApplicationData.js";
-import MentorList from '../Mentor/MentorList';
-import MentorListItem from '../Mentor/MentorListItem';
-
-
-export default function Menteefeed(props) {
-  
-  const data = props.state.mentors;
-  const [allData, setData] = useState(data);
-  
-
-  
-
-  const generateLocationDataForDropdown = () => {
-    return [...new Set(data.map((item) => item.location))];
-  };
-
-  const handleFilterName = (name) => {
-    const filteredData = data.filter((item) => {
-      const fullName = `${item.first_name} ${item.last_name}`;
-      if (fullName.toLowerCase().includes(name.toLowerCase())) {
-        return item;
-      }
-    });
-
-    setData(filteredData);
-  };
-
-  const handleFilterSkill = (skill) => {
-    const filteredData = data.filter((item) => {
-      if (item.skill.toLowerCase().includes(skill.toLowerCase())) {
-        return item;
-      }
-    });
-
-    setData(filteredData);
-  };
-
-  const handleFilterLocation = (location) => {
-    const filteredData = data.filter((item) => {
-      if (item.location === location) {
-        return item;
-      }
-    });
-
-    setData(filteredData);
-  };  
-
+import React from "react";
+import "./MentorListItem.css";
+export default function MentorListItem({item}) {
+ 
   return (
-    <div className="menteefeed-wrapper"> 
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-3">
-          {/* <Dashboard /> */}
-          <Searchbar 
-            locations={generateLocationDataForDropdown()}
-            onNameFilter={handleFilterName}
-            onSkillFilter={handleFilterSkill}
-            onLocationFilter={handleFilterLocation} 
-          />
-        </div>
-        {/* <Quotes /> */}
-        <div className="col-sm-9">
-          <div className="row mt-5">
-            {allData.map((item) => (
-              <MentorListItem item={item} key={item.id} />
-            ))}
-          </div>
-        </div>
+    <div className='card'>
+      <div className="card_photo">
+        <img style={{height: "20em"}} src={item?.photo_url}></img>
       </div>
-    </div>
+        <b>{item?.first_name} {item?.last_name}</b>
+
+      <div className="card_description">
+        <p>{item?.description}</p>
+      </div>
+      <div className="card_tag">
+        <p>{item?.skill}</p>
+      </div>
+      <div className="card_tag">
+        <p>{item?.location}</p>
+      </div>
+      <div>
+        <button className="card_btn">view profile</button>
+      </div>
     </div>
   );
 }
