a518b66f8f00ccdf3d487cbf9d6326633e4f95f4
carmenkk <carmenkuang87@gmail.com>
2022-04-22 07:19:58 -0400

    Fix error to display name and photo on page

--- client/src/components/Dashboard/Mentor/MentorListItem.jsx
+++ client/src/components/Dashboard/Mentor/MentorListItem.jsx
@@ -7,9 +7,9 @@ export default function MentorListItem({item}) {
   return (
     <div className='card'>
       <div className="card_photo">
-        <img src={item?.photo}></img>
+        <img src={item?.photo_url}></img>
       </div>
-        <h3>{item?.firstname} {item?.lastname}</h3>
+        <h3>{item?.first_name} {item?.last_name}</h3>
 
       <div className="card_description">
         <p>{item?.description}</p>
