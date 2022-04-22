eb7488660671ba5d438e8e6f234ad11201b3cb9b
carmenkk <carmenkuang87@gmail.com>
2022-04-22 06:43:12 -0400

    updates to login page

--- client/src/components/Dashboard/Mentee/Menteefeed.jsx
+++ client/src/components/Dashboard/Mentee/Menteefeed.jsx
@@ -73,13 +73,15 @@ export default function Menteefeed(props) {
     <div className="container">
       <div className="row">
         <div className="col-sm-3">
-          <Searchbar
+          <Dashboard />
+          <Filter />
+          <Searchbar 
             locations={generateLocationDataForDropdown()}
             onNameFilter={handleFilterName}
             onSkillFilter={handleFilterSkill}
-            onLocationFilter={handleFilterLocation}
-           
+            onLocationFilter={handleFilterLocation} 
           />
+          <MentorList />
         </div>
         <Quotes />
         <div className="col-sm-9">
