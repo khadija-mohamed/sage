6df2ea7e7dbbe213fb73d026c0577fce93561d1c
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-23 12:01:19 -0400

    Update room number to be static and clean up code

--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -1,6 +1,6 @@
 import { JitsiMeeting } from "@jitsi/react-sdk";
 import React, { useRef, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import logo from "../Dashboard/logo.svg";
 
@@ -85,11 +85,10 @@ export default function Video() {
 
   const handleReadyToClose = () => {
     /* eslint-disable-next-line no-alert */
-    navigate('/dashboard')
+    navigate("/dashboard");
   };
 
-  const generateRoomName = () =>
-    `JitsiMeetRoomNo${Math.random() * 100}-${Date.now()}`;
+  const generateRoomName = () => `SageRoomNo${3754 * 13674}`;
 
   // Multiple instances demo
   const renderNewInstance = () => {
@@ -105,103 +104,11 @@ export default function Video() {
     );
   };
 
-  // const renderButtons = () => (
-  //   <div style={{ margin: "15px 0" }}>
-  //     <div
-  //       style={{
-  //         display: "flex",
-  //         justifyContent: "center",
-  //       }}
-  //     >
-  //       <button
-  //         type="text"
-  //         title="Click to execute toggle raise hand command"
-  //         style={{
-  //           border: 0,
-  //           borderRadius: "6px",
-  //           fontSize: "14px",
-  //           background: "#f8ae1a",
-  //           color: "#040404",
-  //           padding: "12px 46px",
-  //           margin: "2px 2px",
-  //         }}
-  //         onClick={() => apiRef.current.executeCommand("toggleRaiseHand")}
-  //       >
-  //         Raise hand
-  //       </button>
-  //       <button
-  //         type="text"
-  //         title="Click to approve/reject knocking participant"
-  //         style={{
-  //           border: 0,
-  //           borderRadius: "6px",
-  //           fontSize: "14px",
-  //           background: "#0056E0",
-  //           color: "white",
-  //           padding: "12px 46px",
-  //           margin: "2px 2px",
-  //         }}
-  //         onClick={() =>
-  //           resolveKnockingParticipants(({ name }) => !name.includes("test"))
-  //         }
-  //       >
-  //         Resolve lobby
-  //       </button>
-  //       <button
-  //         type="text"
-  //         title="Click to execute subject command"
-  //         style={{
-  //           border: 0,
-  //           borderRadius: "6px",
-  //           fontSize: "14px",
-  //           background: "#df486f",
-  //           color: "white",
-  //           padding: "12px 46px",
-  //           margin: "2px 2px",
-  //         }}
-  //         onClick={() =>
-  //           apiRef.current.executeCommand("subject", "New Subject")
-  //         }
-  //       >
-  //         Change subject
-  //       </button>
-  //       <button
-  //         type="text"
-  //         title="Click to create a new JitsiMeeting instance"
-  //         style={{
-  //           border: 0,
-  //           borderRadius: "6px",
-  //           fontSize: "14px",
-  //           background: "#3D3D3D",
-  //           color: "white",
-  //           padding: "12px 46px",
-  //           margin: "2px 2px",
-  //         }}
-  //         onClick={() => toggleShowNew(!showNew)}
-  //       >
-  //         Toggle new instance
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
-
-  // const renderLog = () =>
-  //   logItems.map((item, index) => (
-  //     <div
-  //       style={{
-  //         fontFamily: "monospace",
-  //         padding: "5px",
-  //       }}
-  //       key={index}
-  //     >
-  //       {item}
-  //     </div>
-  //   ));
-
   const renderSpinner = () => (
     <div
       style={{
-        fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+        fontFamily:
+          "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
         textAlign: "center",
       }}
     >
@@ -213,14 +120,15 @@ export default function Video() {
     <>
       <h1
         style={{
-          fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+          fontFamily:
+            "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
           textAlign: "center",
           border: "2px solid white",
           margin: "2px",
           background: "rgb(91, 125, 116)",
         }}
       >
-        <img className="o3" src={logo} alt="logo"/>
+        <img className="o3" src={logo} alt="logo" />
       </h1>
       <JitsiMeeting
         roomName={generateRoomName()}
@@ -233,9 +141,7 @@ export default function Video() {
         onReadyToClose={handleReadyToClose}
         getIFrameRef={handleJitsiIFrameRef1}
       />
-      {/* {renderButtons()} */}
       {renderNewInstance()}
-      {/* {renderLog()} */}
     </>
   );
 }
