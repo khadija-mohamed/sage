1585687f490c1e7b28f4ef0500ef935b40103d74
carmenkk <carmenkuang87@gmail.com>
2022-04-23 12:10:13 -0400

    Rearrange the divs and give new class names

--- client/src/components/Dashboard/Mentor/MentorListItem.jsx
+++ client/src/components/Dashboard/Mentor/MentorListItem.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import "./MentorListItem.css";
+import "./MentorListItem.scss";
 export default function MentorListItem({item}) {
  
   return (
-    <div className='card'>
-      <div className="card_photo">
-        <img style={{height: "20em"}} src={item?.photo_url}></img>
-      </div>
-        <b>{item?.first_name} {item?.last_name}</b>
+    <div class="Booking">
+      <div class="booking-container">
+        <div class="card-container">
 
-      <div className="card_description">
-        <p>{item?.description}</p>
+          <div class="card-wrapper">
+            <div class="card dark"
+            style={{backgroundImage:"{item?.photo_url}"}}
+            >
+              <img style={{height: "20em"}} src={item?.photo_url}></img>
+          </div>
+          <div class="text-overlay"></div>
+            <h2>{item?.first_name} {item?.last_name}</h2>
+          <div class="details-text">
+            <span>{item?.description}</span>
+            <span>{item?.skill}</span>
+            <span>{item?.location}</span>
+          </div>
+          </div>
+          <div class="purchase-button-container">
+          <h2 class="back-h2">Team</h2>
+          <div class="purchase-button dark">View Profile</div>
+          </div>
+        </div>
       </div>
-      <div className="card_tag">
-        <p>{item?.skill}</p>
-      </div>
-      <div className="card_tag">
-        <p>{item?.location}</p>
-      </div>
-      <div>
-        <button className="card_btn">view profile</button>
-      </div>
-    </div>
+     </div>
   );
 }
