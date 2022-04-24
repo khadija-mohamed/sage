dae834fe7442fca464ff30edbdbd666fa16ca222
carmenkk <carmenkuang87@gmail.com>
2022-04-24 08:07:27 -0400

    Get all data from mentees

--- server/routes/mentees.js
+++ server/routes/mentees.js
@@ -1,19 +1,13 @@
-const router = require("express").Router();
-
-const bcrypt = require('bcrypt');
-
-const mentees = ["Carmen", "Khadija", "Ashley", "Josh", "Louis"];
+const router = require('express').Router();
 
 module.exports = (db) => {
-  // all routes will go here
-  router.get("/", (req, res) => {
-    res.json(mentees);
-  });
+  router.get("/", (request, response) => {
+    db.query(`SELECT * FROM mentees`).then(({ rows: mentees }) => {
+      response.json(
+       mentees
+      );
 
-  /* GET login page. */
-  router.get("/login", (req, res) => {
-    res.render("login");
+    });
   });
-
   return router;
-};
+}
