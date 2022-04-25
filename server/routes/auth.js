233c6e91a15275be22726311bede3ff4f0d46e02
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-24 23:06:05 -0400

    Clean up code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,7 +10,16 @@ module.exports = (db) => {
       `INSERT INTO mentees(first_name, last_name, location, email, password, photo_url, description, skill)
       VALUES($1, $2, $3, $4, $5, $6, $7, $8)
       RETURNING email`,
-      [user.first_name, user.last_name, user.location, user.email, user.password, user.photo_url, user.description, user.skill]
+      [
+        user.first_name,
+        user.last_name,
+        user.location,
+        user.email,
+        user.password,
+        user.photo_url,
+        user.description,
+        user.skill,
+      ]
     );
     return result.rows[0];
   };
@@ -28,14 +37,31 @@ module.exports = (db) => {
   };
 
   // all routes will go here
-
   router.post("/register/sagee", async (req, res) => {
     let templateVars = {
       user: req.session,
     };
 
-    const { first_name, last_name, email, password, photo_url, location, description, skill } = req.body;
-    if (!first_name || !last_name || !email || !password || !photo_url || !location || !description || !skill) {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      photo_url,
+      location,
+      description,
+      skill,
+    } = req.body;
+    if (
+      !first_name ||
+      !last_name ||
+      !email ||
+      !password ||
+      !photo_url ||
+      !location ||
+      !description ||
+      !skill
+    ) {
       let templateVars = {
         message: "Input fields cannot be blank",
       };
@@ -43,7 +69,6 @@ module.exports = (db) => {
     }
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
-    console.log("hash", hashedPassword)
 
     const input = {
       first_name,
@@ -53,22 +78,20 @@ module.exports = (db) => {
       photo_url,
       location,
       description,
-      skill
-    }
-    console.log("Input", input)
+      skill,
+    };
     try {
-    await registerNewUser(input)
-        let templateVars = {
-          message: "all good",
-        };
-        req.session.email = input.email;
-       res.status(200).json(templateVars);
-        } 
-      catch(err) {
-        console.log(err);
-        res.status(500).send("error");
-      }
-      });
+      await registerNewUser(input);
+      let templateVars = {
+        message: "all good",
+      };
+      req.session.email = input.email;
+      res.status(200).json(templateVars);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("error");
+    }
+  });
 
   router.post("/login", (req, res) => {
     console.log("Session", req.session);
@@ -83,31 +106,25 @@ module.exports = (db) => {
       return res.status(400).json(templateVars);
     }
 
-    console.log("Email", email);
-
     // use email to find user in db
-    potentialLogin(email)
-    .then((user) => {
+    potentialLogin(email).then((user) => {
       let templateVars = {
         message: "Email and/or password is incorrect",
         email: req.session.email,
       };
-      // console.log("User", user);
-      if(!user) {
+      if (!user) {
         return res.status(400).json(templateVars);
       }
       // compare password
-      const correctPassword = bcrypt.compareSync(password, user.password)
-      // console.log("Correct Password", correctPassword);
-      if(correctPassword) {
-        req.session.email = user.email
-        console.log("Req.session", req.session);
+      const correctPassword = bcrypt.compareSync(password, user.password);
+      if (correctPassword) {
+        req.session.email = user.email;
         return res.status(204).send();
       } else {
         return res.status(400).json(templateVars);
       }
-    })
-  })
+    });
+  });
 
   return router;
 };
