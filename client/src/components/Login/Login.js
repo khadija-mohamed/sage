06dfb0d7c3d5cc69adf75a99194ccd174283541f
Carmen Kuang <carmen@Carmens-MacBook-Air.local>
2022-04-25 11:15:32 -0400

    editing profile

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,19 +1,131 @@
 
-import React, { useContext, useState } from 'react';
+// import React, { useContext, useState } from 'react';
+// import axios from "axios";
+// import { useNavigate } from 'react-router-dom';
+// import './Login.css';
+// import useApplicationData from "../../hooks/useApplicationData";
+
+
+// import { UserContext } from '../../UserContext';
+
+
+// import logo from "./logo.svg";
+
+// import "./Login.css";
+// import NavBar from "../NavBar/NavBar";
+
+
+// export default function Login(props) {
+//    const { setIsLoggedIn, onUpdate } = props;
+//   const [email, setEmail] = useState();
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+//   const navigate = useNavigate();
+//   // const [user, setUser] = useState("");
+//   const data = props;
+//  console.log("loginprops",props)
+//   const{user,login} = useContext(UserContext);
+
+ 
+//    const { state } = useApplicationData();
+
+//   const validateForm = () => {
+//     if (!email) {
+//       setError("Email cannot be blank");
+//       return false;
+//     }
+//     setError("");
+//     if (!password) {
+//       setError("Password cannot be blank");
+//       return false;
+//     }
+//     return true;
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+// // if -- err.response exist -- do in catch function, send unknown error otherwise .
+
+//     if (validateForm()) {
+//       axios
+//         .post(
+//           'http://localhost:8080/login/login',
+//           {
+//             email: email,
+//             password: password,
+//           },
+//           { withCredentials: true }
+//         )
+//         .then(() => {
+//           const mentee = state.mentees.map((menteeEmail) => menteeEmail.email);
+//           const mentor = state.mentors.map((mentorEmail) => mentorEmail.email);
+//           console.log("hihihi,mentee",mentee)
+//           if (mentee.includes(email)) {
+//             navigate('/dashboard/mentee/menteefeed');
+//           }
+//           if (mentor.includes(email)) {
+//             navigate('/dashboard/mentor/mentorfeed');
+//           }
+//         })
+//         .catch(err => {
+//           console.log("Error", err.response)
+//           alert(err.response.data.message)     
+//         });
+//     }
+//   }
+
+//   console.log("what is user",user)
+//   return(
+//     <div>
+//       <NavBar/>
+//     <div className="login-wrapper">
+       
+//       {/* <div id="bg"></div> */}
+//       <div className="orange-main">
+//       <div className="transparent-text">
+//       </div>
+//       <div className="sage-block">
+//         <div className="head-main">
+        
+//           <span className="head-left"> S</span>
+//            <img className="o3" src={logo} alt="logo"/> 
+//           <span className="head-right">GE</span>
+//           <div className='login'>
+//             <h4> {" "}Please enter email and Password to proceed.</h4>
+//           {/* <div className='form-input'> */}
+//             <form onSubmit={handleSubmit} action="">
+//                    {/* <label for=""></label>  */}
+//                   <input type="text" name="" id="email" placeholder="email" className="email" onChange={e => setEmail(e.target.value)}></input>
+//                   {/* <label for=""></label> */}
+//                   <input type="password" name="" id="password" placeholder="password" className="pass" onChange={e => setPassword(e.target.value)}></input>
+//                   <button className="button-52" type="submit" onClick={() => login(email)}>Submit</button>
+//           </form>
+//           </div>
+//           </div>
+       
+        
+//       </div>
+//     </div>
+  
+//   </div>
+// </div>
+    
+//   )
+// }
+
+
+
+   
+import React, { useContext, useState } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import './Login.css';
-
-
-
-import { UserContext } from '../../UserContext';
-
-
+import { useNavigate, Link } from "react-router-dom";
+import "./Login.css";
+import { UserContext } from "../../UserContext";
 import logo from "./logo.svg";
 
-import "./Login.css";
 import NavBar from "../NavBar/NavBar";
-
+import useApplicationData from "../../hooks/useApplicationData";
 
 export default function Login(props) {
   const { setIsLoggedIn, onUpdate } = props;
@@ -21,14 +133,10 @@ export default function Login(props) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  // const [user, setUser] = useState("");
 
-  const{user,login} = useContext(UserContext);
-  
+  const { user, login } = useContext(UserContext);
 
-  // const {user:email,password,
-  //        setUser:setEmail,setPassword} = useContext(UserContext);
-  // const [user, setUser] = useState
+  const { state } = useApplicationData();
 
   const validateForm = () => {
     if (!email) {
@@ -46,54 +154,86 @@ export default function Login(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (validateForm) {
-      axios
-        .post("http://localhost:8080/login/login", {
-          email: email,
-          password: password,
-        })
-        .then(() => {
-          navigate("/dashboard");
-        })
-        .catch((err) => err);
-    }
-  };
+// if -- err.response exist -- do in catch function, send unknown error otherwise .
 
-  console.log("what is user",user)
-  return(
+    if (validateForm()) {
+      axios
+        .post(
+          'http://localhost:8080/login/login',
+          {
+            email: email,
+            password: password,
+          },
+          { withCredentials: true }
+        )
+        .then(() => {
+          const mentee = state.mentees.map((menteeEmail) => menteeEmail.email);
+          const mentor = state.mentors.map((mentorEmail) => mentorEmail.email);
+          if (mentee.includes(email)) {
+            navigate('/dashboard/mentee/menteefeed');
+          }
+          if (mentor.includes(email)) {
+            navigate('/dashboard/mentor/mentorfeed');
+          }
+        })
+        .catch(err => {
+           alert(err.response.data.message)     
+        });
+    }
+  }
+
+  return (
     <div>
-      <NavBar/>
-    <div className="login-wrapper">
-       
-      {/* <div id="bg"></div> */}
-      <div className="orange-main">
-      <div className="transparent-text">
-      </div>
-      <div className="sage-block">
-        <div className="head-main">
-        
-          <span className="head-left"> S</span>
-           <img className="o3" src={logo} alt="logo"/> 
-          <span className="head-right">GE</span>
-          <div className='login'>
-            <h4> {" "}Please enter email and Password to proceed.</h4>
-          {/* <div className='form-input'> */}
-            <form onSubmit={handleSubmit} action="">
-                   {/* <label for=""></label>  */}
-                  <input type="text" name="" id="email" placeholder="email" className="email" onChange={e => setEmail(e.target.value)}></input>
-                  {/* <label for=""></label> */}
-                  <input type="password" name="" id="password" placeholder="password" className="pass" onChange={e => setPassword(e.target.value)}></input>
-                  <button className="button-52" type="submit" onClick={() => login(email)}>Submit</button>
-          </form>
+      <NavBar />
+      <div className="login-wrapper">
+        <div className="orange-main">
+          <div className="register-btn">
+        <button className="button-52" type="submit"><Link to="/register"><span className="register-btn-text">Register</span></Link></button>
+        </div>
+          <div className="transparent-text"></div>
+          <div className="sage-block">
+            <div className="head-main">
+              <span className="head-left"> S</span>
+              <img className="o3" src={logo} alt="logo" />
+              <span className="head-right">GE</span>
+              <div className="login">
+                {!email || !password ? (
+                  <div>
+                    <h4> Please enter email and password to proceed.</h4>
+                  </div>
+                ) : (
+                  <h4>“The expert in anything was once a beginner.”</h4>
+                )}
+                <form onSubmit={handleSubmit} action="">
+                  <input
+                    type="text"
+                    name=""
+                    id="email"
+                    placeholder="email"
+                    className="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
+                  <input
+                    type="password"
+                    name=""
+                    id="password"
+                    placeholder="password"
+                    className="pass"
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                  <button
+                    className="button-52"
+                    type="submit"
+                    onClick={() => login(email)}
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
-          </div>
-       
-        
+        </div>
       </div>
-    </div>
-  
-  </div>
-</div>
-    
+    </div> 
   )
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,22 +7,36 @@ const bcrypt = require("bcryptjs");
 module.exports = (db) => {
   const registerNewUser = async (user) => {
     const result = await pool.query(
-      "INSERT INTO mentees(first_name, last_name, location, email, password, photo_url, description, skill) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING email",
-      [user.first_name, user.last_name, user.location, user.email, user.password, user.photo_url, user.description, user.skill]
+      `INSERT INTO mentees(first_name, last_name, location, email, password, photo_url, description, skill)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING email`,
+      [
+        user.first_name,
+        user.last_name,
+        user.location,
+        user.email,
+        user.password,
+        user.photo_url,
+        user.description,
+        user.skill,
+      ]
     );
     return result.rows[0];
   };
 
-  const potentialLogin = async (email, password) => {
+  const potentialLogin = async (email) => {
     const result = await pool.query(
-      "SELECT id AS mentee_id, email, password FROM mentees UNION SELECT id AS mentor_id, email, password FROM mentors WHERE email=$1 AND password=$2",
-      [email, password]
+      `SELECT id AS mentee_id, email, password
+      FROM mentees WHERE email=$1
+      UNION SELECT id AS mentor_id, email, password 
+      FROM mentors WHERE email=$1`,
+      [email]
     );
 
-    const actualUser = result.rows.filter((item) => item.password === password && item.email === email);
-
-    return actualUser[0];
+    return result.rows[0];
   };
+
+  
 
   // all routes will go here
 
@@ -31,8 +45,26 @@ module.exports = (db) => {
       user: req.session,
     };
 
-    const { first_name, last_name, email, password, photo_url, location, description, skill } = req.body;
-    if (!first_name || !last_name || !email || !password || !photo_url || !location || !description || !skill) {
+    const {
+      first_name,
+      last_name,
+      email,
+      password,
+      photo_url,
+      location,
+      description,
+      skill,
+    } = req.body;
+    if (
+      !first_name ||
+      !last_name ||
+      !email ||
+      !password ||
+      !photo_url ||
+      !location ||
+      !description ||
+      !skill
+    ) {
       let templateVars = {
         message: "Input fields cannot be blank",
       };
@@ -49,43 +81,88 @@ module.exports = (db) => {
       photo_url,
       location,
       description,
-      skill
-    }
-    console.log("Input", input)
-    try {
-    await registerNewUser(input)
-        let templateVars = {
-          message: "all good",
-        };
-        req.session.email = input.email;
-       res.status(200).json(templateVars);
-        } 
-      catch(err) {
-        console.log(err);
-        res.status(500).send("error");
-      }
-      });
-
-  router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    let templateVars = {
-      message: "Email or password is incorrect",
-      user: req.session,
-      password,
+      skill,
     };
-    if (!email || !password) {
+    try {
+      await registerNewUser(input);
       let templateVars = {
-        message: "Input fields cannot be blank",
-        user: req.session,
+        message: "all good",
       };
-      return res.status(400).json(templateVars);
+      req.session.email = input.email;
+      res.status(200).json(templateVars);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("error");
     }
-    const result = bcrypt.compareSync(password, templateVars.password)
-    if (!result) {
-      return res.send("Email or password is incorrect")
-    }
-    req.session.email = user.id
   });
 
-  return router;
-};
+      router.post("/login", (req, res) => {
+        // check email and password
+        const { email, password } = req.body;
+    
+        if (!email || !password) {
+          let templateVars = {
+            message: "Input fields cannot be blank",
+            user: req.session,
+          };
+          return res.status(400).json(templateVars);
+        }
+        // console.log("herrrrr",user,req.session)
+        // use email to find user in db
+        potentialLogin(email).then((user) => {
+          let templateVars = {
+            message: "Email and/or password is incorrect",
+            email: req.session.email,
+          };
+          if (!user) {
+            return res.status(400).json(templateVars);
+          }
+          // compare password
+          const correctPassword = bcrypt.compareSync(password, user.password);
+          if (correctPassword) {
+            req.session.email = user.email;
+            return res.status(204).send();
+          } else {
+            return res.status(400).send();
+          }
+        });
+        
+      });
+    
+      return router;
+    };
+
+//edit mentee profile
+
+router.get("/mentee/:id/edit", (req, res) => {
+
+  const {id} = req.params;
+ 
+  pool.query(`SELECT * FROM mentees WHERE id = $1`, [id])
+      .then(data => {
+        
+   
+      return res.json(data.rows[0]);
+  });
+});
+
+
+router.post("/mentee/:id/edit", (req, res) => {
+  let templateVars = {
+    user: req.session
+  }
+  const {photo_url, location, description, skill,isactive } = req.body
+  
+
+
+ pool.query( `Update mentees SET  location = $1, description = $2, skill = $3, photo_url = $4, isactive = $5`,[location, description, skill,photo_url, isactive])
+ .then(() => {
+   console.log('item was added edited');
+  res.send("whhhh")
+  
+});
+
+return router;
+ 
+});
+
