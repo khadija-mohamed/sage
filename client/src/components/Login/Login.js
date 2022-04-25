87883d5b286b7ecc276f1c4474ef33861a20fe6d
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-24 22:55:42 -0400

    Add conditionals to display different pages if a mentee or mentor is logged in

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,19 +1,14 @@
-
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
-
-
-import { UserContext } from '../../UserContext';
-
+import { UserContext } from "../../UserContext";
 
 import logo from "./logo.svg";
 
-import "./Login.css";
 import NavBar from "../NavBar/NavBar";
-
+import useApplicationData from "../../hooks/useApplicationData";
 
 export default function Login(props) {
   const { setIsLoggedIn, onUpdate } = props;
@@ -21,14 +16,10 @@ export default function Login(props) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  // const [user, setUser] = useState("");
 
-  const{user,login} = useContext(UserContext);
-  
+  const { user, login } = useContext(UserContext);
 
-  // const {user:email,password,
-  //        setUser:setEmail,setPassword} = useContext(UserContext);
-  // const [user, setUser] = useState
+  const { state } = useApplicationData();
 
   const validateForm = () => {
     if (!email) {
@@ -48,52 +39,77 @@ export default function Login(props) {
 
     if (validateForm()) {
       axios
-        .post("http://localhost:8080/login/login", {
-          email: email,
-          password: password,
-        }, {withCredentials: true})
+        .post(
+          "http://localhost:8080/login/login",
+          {
+            email: email,
+            password: password,
+          },
+          { withCredentials: true }
+        )
         .then(() => {
-          navigate("/dashboard");
+          const mentee = state.mentees.map((menteeEmail) => menteeEmail.email);
+          const mentor = state.mentors.map((mentorEmail) => mentorEmail.email);
+          if (mentee.includes(email)) {
+            navigate("/dashboard/mentee/menteefeed");
+          }
+          if (mentor.includes(email)) {
+            navigate("/dashboard/mentor/mentorfeed");
+          }
         })
         .catch((err) => err);
     }
   };
 
-  console.log("what is user",user)
-  return(
+  return (
     <div>
-      <NavBar/>
-    <div className="login-wrapper">
-       
-      {/* <div id="bg"></div> */}
-      <div className="orange-main">
-      <div className="transparent-text">
-      </div>
-      <div className="sage-block">
-        <div className="head-main">
-        
-          <span className="head-left"> S</span>
-           <img className="o3" src={logo} alt="logo"/> 
-          <span className="head-right">GE</span>
-          <div className='login'>
-          {!email || !password ? <div><h4> Please enter email and Password to proceed.</h4></div> : <h4>“The expert in anything was once a beginner.”</h4>}
-          {/* <div className='form-input'> */}
-            <form onSubmit={handleSubmit} action="">
-                   {/* <label for=""></label>  */}
-                  <input type="text" name="" id="email" placeholder="email" className="email" onChange={e => setEmail(e.target.value)}></input>
-                  {/* <label for=""></label> */}
-                  <input type="password" name="" id="password" placeholder="password" className="pass" onChange={e => setPassword(e.target.value)}></input>
-                  <button className="button-52" type="submit" onClick={() => login(email)}>Submit</button>
-          </form>
+      <NavBar />
+      <div className="login-wrapper">
+        <div className="orange-main">
+          <div className="transparent-text"></div>
+          <div className="sage-block">
+            <div className="head-main">
+              <span className="head-left"> S</span>
+              <img className="o3" src={logo} alt="logo" />
+              <span className="head-right">GE</span>
+              <div className="login">
+                {!email || !password ? (
+                  <div>
+                    <h4> Please enter email and password to proceed.</h4>
+                  </div>
+                ) : (
+                  <h4>“The expert in anything was once a beginner.”</h4>
+                )}
+                <form onSubmit={handleSubmit} action="">
+                  <input
+                    type="text"
+                    name=""
+                    id="email"
+                    placeholder="email"
+                    className="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
+                  <input
+                    type="password"
+                    name=""
+                    id="password"
+                    placeholder="password"
+                    className="pass"
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                  <button
+                    className="button-52"
+                    type="submit"
+                    onClick={() => login(email)}
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
-          </div>
-       
-        
+        </div>
       </div>
     </div>
-  
-  </div>
-</div>
-    
-  )
+  );
 }
