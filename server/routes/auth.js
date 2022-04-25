c0571962bb7cf192dc48e3664560927d969eae1d
Sasu Osayande <osayande.sasu@outlook.com>
2022-04-24 22:03:20 -0400

    Fix query and logic for login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,21 +7,24 @@ const bcrypt = require("bcryptjs");
 module.exports = (db) => {
   const registerNewUser = async (user) => {
     const result = await pool.query(
-      "INSERT INTO mentees(first_name, last_name, location, email, password, photo_url, description, skill) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING email",
+      `INSERT INTO mentees(first_name, last_name, location, email, password, photo_url, description, skill)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING email`,
       [user.first_name, user.last_name, user.location, user.email, user.password, user.photo_url, user.description, user.skill]
     );
     return result.rows[0];
   };
 
-  const potentialLogin = async (email, password) => {
+  const potentialLogin = async (email) => {
     const result = await pool.query(
-      "SELECT id AS mentee_id, email, password FROM mentees UNION SELECT id AS mentor_id, email, password FROM mentors WHERE email=$1 AND password=$2",
-      [email, password]
+      `SELECT id AS mentee_id, email, password
+      FROM mentees WHERE email=$1
+      UNION SELECT id AS mentor_id, email, password 
+      FROM mentors WHERE email=$1`,
+      [email]
     );
 
-    const actualUser = result.rows.filter((item) => item.password === password && item.email === email);
-
-    return actualUser[0];
+    return result.rows[0];
   };
 
   // all routes will go here
@@ -40,6 +43,7 @@ module.exports = (db) => {
     }
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
+    console.log("hash", hashedPassword)
 
     const input = {
       first_name,
@@ -67,12 +71,10 @@ module.exports = (db) => {
       });
 
   router.post("/login", (req, res) => {
+    console.log("Session", req.session);
+    // check email and password
     const { email, password } = req.body;
-    let templateVars = {
-      message: "Email or password is incorrect",
-      user: req.session,
-      password,
-    };
+
     if (!email || !password) {
       let templateVars = {
         message: "Input fields cannot be blank",
@@ -80,11 +82,85 @@ module.exports = (db) => {
       };
       return res.status(400).json(templateVars);
     }
-    const result = bcrypt.compareSync(password, templateVars.password)
-    if (!result) {
-      return res.send("Email or password is incorrect")
+
+    console.log("Email", email);
+
+    // use email to find user in db
+    potentialLogin(email)
+    .then((user) => {
+      let templateVars = {
+        message: "Email and/or password is incorrect",
+        email: req.session.email,
+      };
+      // console.log("User", user);
+      if(!user) {
+        return res.status(400).json(templateVars);
+      }
+      // compare password
+      const correctPassword = bcrypt.compareSync(password, user.password)
+      // console.log("Correct Password", correctPassword);
+      if(correctPassword) {
+        req.session.email = user.email
+        console.log("Req.session", req.session);
+        return res.status(204).send();
+      } else {
+        return res.status(400).json(templateVars);
+      }
+    })
+  })
+
+  router.post("/login2", (req, res) => {
+    const { email, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    console.log("hash", hashedPassword)
+
+    let templateVars = {
+      message: "Email or password is incorrect",
+      user: req.session,
+      password: hashedPassword,
+    };
+    console.log("hashed", templateVars.password)
+    console.log("password", password)
+    if (!email || !password) {
+      let templateVars = {
+        message: "Input fields cannot be blank",
+        user: req.session,
+      };
+      return res.status(400).json(templateVars);
     }
-    req.session.email = user.id
+    potentialLogin(email, password)
+      .then((user) => {
+        let templateVars = {
+          message: "Email and/or password is incorrect",
+          user: req.session,
+          password: hashedPassword,
+        };
+        const result = bcrypt.compareSync(password, templateVars.password)
+        console.log("result", result);
+        if (!user) {
+          let templateVars = {
+            message: "Email and/or password is incorrect",
+            user: req.session,
+          };
+          return res.status(400).json(templateVars);
+        } 
+        if (!result) {
+          return res.status(400).json(templateVars);
+        } else {
+          req.session.email = user.id
+          return res.json({});
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+      });
+    // const result = bcrypt.compareSync(password, templateVars.password)
+    // if (!result) {
+    //   return res.send("Email or password is incorrect")
+    // }
+    // req.session.email = user.id
   });
 
   return router;
