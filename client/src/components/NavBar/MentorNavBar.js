69cdadeb4cab1689b9a0fcf19b6c7de164ff8cbc
Sasu Osayande <osayande.sasu@outlook.com>
2022-06-03 19:33:08 -0400

    Change var to const and change the component name to avoid duplicate component names

--- client/src/components/NavBar/MentorNavBar.js
+++ client/src/components/NavBar/MentorNavBar.js
@@ -3,14 +3,14 @@ import "./NavBar.css";
 import { NavLink } from "react-router-dom";
 import $ from "jquery";
 
-const Navbar = () => {
+const MentorNavBar = () => {
   function animation() {
-    var tabsNewAnim = $("#navbarSupportedContent");
-    var activeItemNewAnim = tabsNewAnim.find(".active");
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
+    const tabsNewAnim = $("#navbarSupportedContent");
+    const activeItemNewAnim = tabsNewAnim.find(".active");
+    const activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+    const activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+    const itemPosNewAnimTop = activeItemNewAnim.position();
+    const itemPosNewAnimLeft = activeItemNewAnim.position();
     $(".hori-selector").css({
       top: itemPosNewAnimTop.top + "px",
       left: itemPosNewAnimLeft.left + "px",
@@ -20,10 +20,10 @@ const Navbar = () => {
     $("#navbarSupportedContent").on("click", "li", function (e) {
       $("#navbarSupportedContent ul li").removeClass("active");
       $(this).addClass("active");
-      var activeWidthNewAnimHeight = $(this).innerHeight();
-      var activeWidthNewAnimWidth = $(this).innerWidth();
-      var itemPosNewAnimTop = $(this).position();
-      var itemPosNewAnimLeft = $(this).position();
+      const activeWidthNewAnimHeight = $(this).innerHeight();
+      const activeWidthNewAnimWidth = $(this).innerWidth();
+      const itemPosNewAnimTop = $(this).position();
+      const itemPosNewAnimLeft = $(this).position();
       $(".hori-selector").css({
         top: itemPosNewAnimTop.top + "px",
         left: itemPosNewAnimLeft.left + "px",
@@ -107,4 +107,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default MentorNavBar;
